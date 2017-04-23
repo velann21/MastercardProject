5d86c93301a6cfb9c3d92b084630192e8f743963
SingaravelanNandakumar <velann21@gmail.com>
2017-04-23 07:52:19 +0530

    This is an my first commit

---
+++ CommandLine.java
@@ -0,0 +1,13 @@
+public class CommandLine {
+
+	public static void main(String[] args) {
+		
+		
+		String s1=args[0];
+		String s2=args[1];
+		String s3=args[2];
+
+		System.out.println(s3);
+	}
+
+}
